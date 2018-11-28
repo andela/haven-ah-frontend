feceee7faf78796f7e04e336e23f63734970879e
Uchechukwu Akogwu <akogwuuche@ymail.com>
2018-11-28 10:04:31 +0100

    ch(sass/scss): Setup SASS processing on the client
    Install and setup sass pre-processing to enable the use of both .scss and .css files on the client

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -10,8 +10,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /(\.scss$|\.css$)/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(jpe?g|gif|png)$/,

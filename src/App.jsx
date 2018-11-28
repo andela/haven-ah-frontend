25fa5fac5dd9dd16b3d8f647d797091b6427e280
Uchechukwu Akogwu <akogwuuche@ymail.com>
2018-11-28 10:05:58 +0100

    ch(react-routers): Setup react router version 4 (React-router-dom)
    install and configure react router creating a dummy route for homepage
    create a component to test the route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
 import '../public/styles/App.css';
 
 class App extends Component {
   render() {
     return (
-            <div>
-                <h1>Authors Haven App!</h1>
-            </div>
+      <Router>
+        <Routes />
+      </Router>
     );
   }
 }

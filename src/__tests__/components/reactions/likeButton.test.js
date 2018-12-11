c558788a2ce47f627a0381d2e9549d257ac94d93
PriscillaSam <priscillasamiduh@gmail.com>
2018-12-11 08:21:32 +0100

    feat(comments): get comments on the single article page - write comment form and card components - write actions, reducers, and sagas to handle API calls and data - write tests for all functionality - write modal to display comment replies
    [Finishes #161342806]
    
    (cherry picked from commit 0df348f74aa4707648df9a23c93be6397d197599)

--- src/__tests__/components/reactions/likeButton.test.js
+++ src/__tests__/components/reactions/likeButton.test.js
@@ -3,6 +3,8 @@ import { cleanup, fireEvent, render } from 'react-testing-library';
 import { BrowserRouter as Router } from 'react-router-dom';
 import LikeButton
   from '../../../components/containers/reactions/likeButton';
+// import LikeButton from '../../../components/containers/reactions/LikeButton';
+
 import store from '../../testUtilities/store';
 import {
   articleThread,
@@ -40,4 +42,58 @@ describe('LikeButton Component', () => {
 
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
+  // describe('Like button component', () => {
+  //   it('should remain same on hover if user has liked previously', () => {
+  //     const addIconClass = jest.fn();
+
+  //     const {
+  //       getByText,
+  //       getByTestId
+  //     } = render(<LikeButton hasLiked likes="0" />);
+
+  //     const likes = getByText('0');
+  //     const icon = getByTestId('like-icon');
+
+  //     expect(likes).toBeDefined();
+
+  //     icon.onmouseover = addIconClass;
+  //     fireEvent.mouseOver(icon);
+  //     expect(icon.className).toEqual('fa fa-thumbs-up');
+  //   });
+
+  //   it('should remain same on mouse out if user has liked previously', () => {
+  //     const removeIconClass = jest.fn();
+
+  //     const { getByTestId } = render(<LikeButton hasLiked likes="0" />);
+  //     const icon = getByTestId('like-icon');
+
+  //     icon.onmouseout = removeIconClass;
+  //     fireEvent.mouseOut(icon);
+  //     expect(icon.className).toEqual('fa fa-thumbs-up');
+  //   });
+
+  //   it('should turn dark on hover if user has not liked previously', () => {
+  //     const addIconClass = jest.fn();
+
+  //     const { getByTestId } = render(<LikeButton hasLiked={false} likes="1" />);
+  //     const icon = getByTestId('like-icon');
+
+  //     icon.onmouseover = addIconClass;
+  //     fireEvent.mouseOver(icon);
+
+  //     expect(icon.className).toEqual('fa fa-thumbs-up');
+  //   });
+
+  //   it('should remove dark on mouse out if user has not liked previously', () => {
+  //     const removeIconClass = jest.fn();
+
+  //     const { getByTestId } = render(<LikeButton hasLiked={false} likes="1" />);
+  //     const icon = getByTestId('like-icon');
+
+  //     icon.onmouseout = removeIconClass;
+  //     fireEvent.mouseOut(icon);
+
+  //     expect(icon.className).toEqual('fa fa-thumbs-o-up');
+  //   });
+  // });
 });
